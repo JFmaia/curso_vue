a805b027959961808d5185c5a7691b63a2e0f5b3
JFmaia <jfmaia741@gmail.com>
2022-11-10 10:02:05 -0300

    chore: Desafio completado

---
+++ componentes-avancado/comp-avancado-desafio/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+	<div id="app">
+		<span>
+			<button @click="componente='Vermelho'" class="vermelho">Carregar Componente Vermelho</button>
+			<button @click="componente='Verde'" class="verde">Carregar Componente Verde</button>
+			<button @click="componente='Azul'" class="azul">Carregar Componente Azul</button>
+		</span>
+		<keep-alive>
+			<component :is="componente">
+				<p>ESTE COMPONETE É : <strong>{{componente}}</strong></p>
+			</component>
+		</keep-alive>
+	</div>
+</template>
+
+<script>
+import Vermelho from './components/Vermelho.vue'
+import Verde from './components/Verde.vue'
+import Azul from './components/Azul.vue'
+
+export default {
+	name: 'app',
+	components: { Vermelho, Verde, Azul },
+	data(){
+		return{
+			componente: "Vermelho"
+		}
+	}
+}
+</script>
+
+<style>
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+
+	button {
+		padding: 10px;
+		color: #FFF;
+		font-size: 1.3rem;
+	}
+
+    .caixa {
+        color: #FFF;
+        font-size: 1.8rem;
+        padding: 25px;
+		margin: 10px 0px;
+    }
+
+	.vermelho {
+        border: 2px solid red;
+        background-color: #f54235;
+    }
+
+	.verde {
+        border: 2px solid green;
+        background-color: #49b057;
+    }
+
+	.azul {
+        border: 2px solid blue;
+        background-color: #2594f0;
+    }
+</style>
