ec951f78c3e62fd902dab358e7a939fdb3cb0399
JFmaia <jfmaia741@gmail.com>
2022-11-18 11:15:32 -0300

    chore: Criando diretiva personalizada.

--- diretivas/diretivas-exercicios/src/App.vue
+++ diretivas/diretivas-exercicios/src/App.vue
@@ -6,6 +6,8 @@
 		<p v-text="'Usando ddiretiva v-text'"></p>
 		<!--PEGA o texto ou codigo com html e executa as tags htmls-->
 		<p v-html="'Usando diretiva <strong>v-html</strong>'"></p>
+		<hr>
+		<p v-destaque>Usando diretiva personalizada</p>
 	</div>
 </template>
 
