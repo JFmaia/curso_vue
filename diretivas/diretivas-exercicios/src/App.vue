0ded258d03744b6879de9d5e89c2c8b0bad64411
JFmaia <jfmaia741@gmail.com>
2022-11-21 08:15:07 -0300

    chore: Utilizando diretivas locais.

--- diretivas/diretivas-exercicios/src/App.vue
+++ diretivas/diretivas-exercicios/src/App.vue
@@ -9,11 +9,33 @@
 		<hr>
 		<p v-destaque:fundo.atrasar="cor">Usando diretiva personalizada</p>
 		<p v-destaque="cor">Usando diretiva personalizada</p>
+		<hr>
+		<p v-destaque-local:fundo.atrasar="cor">Usando diretiva personalizada</p>
+		<p v-destaque-local="cor">Usando diretiva personalizada</p>
 	</div>
 </template>
 
 <script>
 export default {
+	directives:{
+		'destaque-local':{
+			
+			bind(el,binding){
+					
+				let atraso = 0
+	
+				if(binding.modifiers['atrasar']) atraso = 3000
+	
+				setTimeout(()=>{
+					if(binding.arg === 'fundo'){
+						el.style.backgroundColor = binding.value
+					}else{
+						el.style.color= binding.value
+					}
+				},atraso)
+			}
+		}
+	},
 	data(){
 		return{
 			cor: 'lightblue'
