26718d16f1a8ec6aed8d9026de2e09240334087a
JFmaia <jfmaia741@gmail.com>
2022-11-23 08:39:56 -0300

    chore: Utilizando filtros.

--- filtros-mixins/filtros-mixins-exercicios/src/App.vue
+++ filtros-mixins/filtros-mixins-exercicios/src/App.vue
@@ -2,12 +2,22 @@
 	<div id="app">
 		<h1>Filtros & Mixins</h1>
 		<hr>
-		<p>{{cpf}}</p>
+		<p>{{cpf | cpf()}}</p>
 	</div>
 </template>
 
 <script>
 export default {
+	//Filtros servem para modificar valores de uma variavel, podendo ser utilizados como mascaras.
+	filters:{
+		cpf(valor){
+			const arr = valor.split('')
+			arr.splice(3,0,'.')
+			arr.splice(7,0,'.')
+			arr.splice(11,0,'-')
+			return arr.join('')
+		}
+	},
 	data(){
 		return{
 			cpf: "08220675446"
