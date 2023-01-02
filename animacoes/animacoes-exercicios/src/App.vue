314117d6caa8e7e611a9dd68ff5983b949023dc6
JFmaia <jfmaia741@gmail.com>
2023-01-02 09:35:52 -0300

    feat: Primeira animação.

--- animacoes/animacoes-exercicios/src/App.vue
+++ animacoes/animacoes-exercicios/src/App.vue
@@ -4,7 +4,7 @@
 		<hr>
 		<b-button variant="primary" class="mb-4" @click="exibir = !exibir">Mostrar Mensagem</b-button>
 
-		<transition>
+		<transition name="fade">
 			<b-alert variant="info" show v-if="exibir">{{msg}}</b-alert>
 		</transition>
 	</div>
@@ -32,4 +32,13 @@ export default {
 	margin-top: 60px;
 	font-size: 1.5rem;
 }
+
+.fade-enter, .fade-leave-to{
+	opacity: 0;
+}
+
+.fade-enter-active, .fade-leave-active {
+	transition: opacity 1s;
+}
+
 </style>
