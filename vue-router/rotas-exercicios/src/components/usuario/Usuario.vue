7f5a51f49d25c994e6b407a4f3c084e17977c6b5
JFmaia <jfmaia741@gmail.com>
2023-01-20 10:30:54 -0300

    feat: Utilizando a navegação por meio eventos como o @click.

--- vue-router/rotas-exercicios/src/components/usuario/Usuario.vue
+++ vue-router/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -1,12 +1,18 @@
 <template>
    <div class="usuario">
     <h2>Usuários</h2>
+    <hr>
+    <button sucesso @click="irParaInicio">Voltar</button>
    </div>
 </template>
 
 <script>
 export default {
-    
+    methods: {
+      irParaInicio(){
+         this.$router.push('/');
+      }
+    },
 }
 </script>
 
