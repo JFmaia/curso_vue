369501c4b9f160d8e3256a6915ea0fff24d9a347
JFmaia <jfmaia741@gmail.com>
2023-01-20 10:11:11 -0300

    feat: Criando um menu para utilizar routes-link.

--- vue-router/rotas-exercicios/src/App.vue
+++ vue-router/rotas-exercicios/src/App.vue
@@ -1,14 +1,16 @@
 <template>
 	<div id="app">
 		<h1>Rotas com VueRouter</h1>
+		<Menu/>
 		<!--Adicionando a entrada das rotas no App-->
 		<router-view />
 	</div>
 </template>
 
 <script>
+import Menu from './components/template/Menu.vue'
 export default {
-	
+	components: { Menu },
 }
 </script>
 
