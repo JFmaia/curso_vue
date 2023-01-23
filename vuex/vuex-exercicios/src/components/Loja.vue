dc4dbdfb9f31e87bb5af932fdef280c5042fcb8c
JFmaia <jfmaia741@gmail.com>
2023-01-23 14:08:50 -0300

    feat: Criando um gerenciador de estado o 'store' e criando um estado central.

---
+++ vuex/vuex-exercicios/src/components/Loja.vue
@@ -0,0 +1,53 @@
+<template>
+    <Painel titulo="Loja Virtual" verde>
+        <div class="loja">
+            <span>Adicionar</span>
+            <input type="number" v-model.number="quantidade">
+            <span>itens de <strong>R$</strong></span>
+            <input type="number" v-model.number="preco">
+            <button @click="adicionar">Agora!</button>
+        </div>
+    </Painel>
+</template>
+
+<script lang="js">
+
+export default {
+    data() {
+        return {
+            sequencia: 1,
+            quantidade: 1,
+            preco: 9.99,
+        }
+    },
+    methods: {
+        adicionar() {
+            const produto = {
+                id: this.sequencia,
+                nome: `Produto ${this.sequencia}`,
+                quantidade: this.quantidade,
+                preco: this.preco
+            }
+            this.sequencia++
+            //Adicionando no stado central do vuex, onde vai atualizar todos os outros componentes.
+            this.$store.state.produtos.push(produto);
+        }
+    }
+}
+</script>
+
+<style>
+    .loja {
+        display: flex;
+        justify-content: center;
+    }
+
+    .loja > * {
+        margin: 0px 10px;
+    }
+
+    input {
+        font-size: 2rem;
+        width: 90px;
+    }
+</style>
